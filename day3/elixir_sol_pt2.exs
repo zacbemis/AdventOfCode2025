107fbf657b85682a1cffefb4105ec05b3c0bc89b
Zacbemis <zac.bemis916@gmail.com>
2025-12-15 11:56:52 -0800

    pt2

--- day3/elixir_sol_pt2.exs
+++ day3/elixir_sol_pt2.exs
@@ -1,4 +1,5 @@
 defmodule Day3 do
+  @num_digits 12
   def solve(file_path) do
     file_path
     |> File.read!()
@@ -10,23 +11,13 @@ defmodule Day3 do
   defp logic(line, total) do
     line_highest = String.graphemes(line)
     |> Enum.map(&String.to_integer/1)
-    |> find_max_pair()
+    |> find_highest_num()
     total + line_highest
   end
 
-  defp find_max_pair([first | rest]) do
-    {best, _best_first} =
-      Enum.reduce(rest, {0, first}, &is_greater/2)
-    best
-  end
-
-  defp is_greater(num, {best, best_first}) do
-    new_num = best_first * 10 + num
-    new_best = max(new_num, best)
-    new_first = max(num, best_first)
-    {new_best, new_first}
+  defp find_highest_num(line) do
+    
   end
 end
-
 total = Day3.solve("day3/input.txt")
 IO.puts("Total: #{total}")
